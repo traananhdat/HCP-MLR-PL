d6ce4764bfc12a3e03222ea6b1a27a0a56f54fff
traananhdat <160739786+traananhdat@users.noreply.github.com>
2024-02-21 23:37:37 +0700

    Update dataloader.py
    images/

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -49,8 +49,8 @@ def get_data_path(dataset):
 
     if dataset == 'COCO2014':
         prefixPath = prefixPathCOCO
-        train_dir, train_anno, train_label = os.path.join(prefixPath, 'train2014'), os.path.join(prefixPath, 'annotations/instances_train2014.json'), './data/coco/train_label_vectors.npy'
-        test_dir, test_anno, test_label = os.path.join(prefixPath, 'val2014'), os.path.join(prefixPath, 'annotations/instances_val2014.json'), './data/coco/val_label_vectors.npy'
+        train_dir, train_anno, train_label = os.path.join(prefixPath, 'images/train2014'), os.path.join(prefixPath, 'annotations/instances_train2014.json'), './data/coco/train_label_vectors.npy'
+        test_dir, test_anno, test_label = os.path.join(prefixPath, 'images/val2014'), os.path.join(prefixPath, 'annotations/instances_val2014.json'), './data/coco/val_label_vectors.npy'
 
     elif dataset == 'VG':
         prefixPath = prefixPathVG
